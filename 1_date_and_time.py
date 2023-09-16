ac759880267b046d5a2bcd8a7f513d1947633e87
stn-01 <p17098807@gmail.com>
2023-09-17 02:47:19 +0300

    homework week 2 pt 2

--- 1_date_and_time.py
+++ 1_date_and_time.py
@@ -5,23 +5,21 @@
 
 1. Напечатайте в консоль даты: вчера, сегодня, 30 дней назад
 2. Превратите строку "01/01/20 12:10:03.234567" в объект datetime
-
 """
+import datetime
 
 def print_days():
-    """
-    Эта функция вызывается автоматически при запуске скрипта в консоли
-    В ней надо заменить pass на ваш код
-    """
-    pass
+    print(f"Вчера: {(datetime.datetime.now() - datetime.timedelta(days=1)).strftime('%d.%m.%Y')}; "
+          f"Сегодня: {datetime.datetime.now().strftime('%d.%m.%Y')}; "
+          f"30 дней назад: {(datetime.datetime.now() - datetime.timedelta(days=30)).strftime('%d.%m.%Y')}")
 
 
 def str_2_datetime(date_string):
-    """
-    Эта функция вызывается автоматически при запуске скрипта в консоли
-    В ней надо заменить pass на ваш код
-    """
-    pass
+    date_and_time = date_string.split()
+    d, m, y = date_and_time[0].split('/')
+    mn, sc, ms_mcs = date_and_time[1].split(':')
+    ms, mcs = ms_mcs.split('.')
+    return datetime.datetime(int(y)+2000, int(m), int(d), int(mn), int(sc), int(ms), int(mcs))
 
 if __name__ == "__main__":
     print_days()
--- 2_files.py
+++ 2_files.py
@@ -12,11 +12,18 @@
 """
 
 def main():
-    """
-    Эта функция вызывается автоматически при запуске скрипта в консоли
-    В ней надо заменить pass на ваш код
-    """
-    pass
+
+    with open('referat.txt', encoding='utf-8') as file:
+        content = file.read()
+        content_in_one_string = content.replace('\n', '')
+        print(f"Длина строки: {len(content_in_one_string)}")
+        print(f"Кол-во слов: {len(content_in_one_string.split(' '))}")
+
+        new_content = content.replace('.', '!')
+
+    with open('referat2.txt', 'w', encoding='utf-8') as file_2:
+        file_2.write(new_content)
+
 
 if __name__ == "__main__":
     main()
